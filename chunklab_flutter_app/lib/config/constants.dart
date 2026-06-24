acd157b5d0b9cd6ecbdb4d04eedb2aa6c00662ce
gulrezqayyum <gulrezqayyum@gmail.com>
2026-06-25 01:46:47 +0500

    flutter work

---
+++ chunklab_flutter_app/lib/config/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String apiBaseUrl = 'http://localhost:8001';
+  static const String queryEndpoint = '/api/documents/query';
+  static const Duration apiTimeout = Duration(seconds: 30);
+
+}
